0475eb9dbd2ae7f9e5540882c7397b795228c43f
noy <noy72@hatena.ne.jp>
2025-12-06 18:28:56 +0900

    chore: install pretteir and eslint

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import eslintPluginAstro from "eslint-plugin-astro";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  eslintPluginAstro.configs.recommended,
+  eslintPluginAstro.configs["flat/jsx-a11y-recommended"],
+]);
