d4659f2f2cc59e862183510fa601baeed877a0fe
noy <noy72@hatena.ne.jp>
2025-12-06 20:23:44 +0900

    chore: .astroは無視する

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
 import js from "@eslint/js";
 import eslintPluginAstro from "eslint-plugin-astro";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
+  globalIgnores([".astro/"]),
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
