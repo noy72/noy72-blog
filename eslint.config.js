3152f3b618adbd2810615314f79a1663a1191eee
noy <noy72@hatena.ne.jp>
2025-12-07 12:39:27 +0900

    chore: extends は使わない

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,11 @@
 import js from "@eslint/js";
 import eslintPluginAstro from "eslint-plugin-astro";
 import { defineConfig, globalIgnores } from "eslint/config";
-import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
   globalIgnores([".astro/"]),
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
-  },
+  js.configs.recommended,
   tseslint.configs.recommended,
   eslintPluginAstro.configs.recommended,
   eslintPluginAstro.configs["flat/jsx-a11y-recommended"],
