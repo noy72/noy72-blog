82ebe5b576236875981df716f6df9292bbe139fa
noy <noy72@hatena.ne.jp>
2026-02-27 21:20:06 +0900

    chore: サイトの description を更新

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,7 +2,7 @@ export const SITE_CONFIG = {
   siteName: "noy72.com",
   baseUrl: import.meta.env.PROD ? "https://noy72.com" : "http://localhost:4321",
   defaultImage: "/og-default.jpg",
-  description: "Web開発とプログラミングに関する技術ブログ",
+  description: "Webアプリケーションエンジニアの趣味と仕事のことを書くブログ",
   author: "noy72",
   gaMeasurementId: "G-NWM51D2Q35",
 } as const;
